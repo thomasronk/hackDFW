d05ef4fd5bc6e58e10abfbd8ebe4abc4e3a73ca1
prashantprakash <prashantpacy@gmail.com>
2015-04-07 00:45:31 -0500

    comments and Stats VO

---
+++ ConnectPeople/src/com/src/techMahindraHack/CommentVO.java
@@ -0,0 +1,53 @@
+package com.src.techMahindraHack;
+
+public class CommentVO {
+
+	private String commentPlace;
+	private String date;
+	private String commentUser;
+	private String tag;
+	private String who;
+	
+	
+
+	public String getCommentPlace() {
+		return commentPlace;
+	}
+
+	public void setCommentPlace(String commentPlace) {
+		this.commentPlace = commentPlace;
+	}
+
+	public String getCommentUser() {
+		return commentUser;
+	}
+
+	public void setCommentUser(String commentUser) {
+		this.commentUser = commentUser;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getWho() {
+		return who;
+	}
+
+	public void setWho(String who) {
+		this.who = who;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
---
+++ ConnectPeople/src/com/src/techMahindraHack/TotalStatVO.java
@@ -0,0 +1,37 @@
+package com.src.techMahindraHack;
+
+public class TotalStatVO {
+	
+	private int nAudio;
+	private int nVideo;
+	private int nMessages;
+	private int nPhone;
+	public int getnAudio() {
+		return nAudio;
+	}
+	public void setnAudio(int nAudio) {
+		this.nAudio = nAudio;
+	}
+	public int getnVideo() {
+		return nVideo;
+	}
+	public void setnVideo(int nVideo) {
+		this.nVideo = nVideo;
+	}
+	public int getnMessages() {
+		return nMessages;
+	}
+	public void setnMessages(int nMessages) {
+		this.nMessages = nMessages;
+	}
+	public int getnPhone() {
+		return nPhone;
+	}
+	public void setnPhone(int nPhone) {
+		this.nPhone = nPhone;
+	}
+	
+	
+	
+
+}
